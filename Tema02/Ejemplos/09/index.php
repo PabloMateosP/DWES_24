929b516a96efb5b0539402040da840df9ae97b0b
unknown <pablomp1301@gmail.com>
2023-09-27 14:15:54 +0200

    Subida a 27/09/23

---
+++ Tema02/Ejemplos/09/index.php
@@ -0,0 +1,16 @@
+<?php 
+    //Constantes
+
+    //Como buena práctica se debe de poner la constante en Mayúscula
+    define("G", 9.81);
+
+    echo G;
+
+    echo "<BR>";
+
+    echo "Numero PI: " + M_PI;
+
+    echo "<BR>";
+
+    echo "Numero PI: " + pi();
+?>

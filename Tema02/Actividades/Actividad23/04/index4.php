4795aa05291391e3f1e7a9af64e39406657d2c43
PabloMateosP <pmatpal0105@g.educaand.es>
2023-10-04 18:01:14 +0200

    Actualización ejercicio 4 del 2.3

--- Tema02/Actividades/Actividad23/04/index4.php
+++ Tema02/Actividades/Actividad23/04/index4.php
@@ -2,22 +2,28 @@
 
 // Ejercicio 4: empty()
 
-$var1 = null;
+$var1 = null; //Valor vacío
 var_dump(empty($var1));
+echo "<BR>";
 
-$var2 = 42;
+$var2 = 42; //Valor no vacío
 var_dump(empty($var2));
+echo "<BR>";
 
-$var3 = "Hola";
+$var3 = "Pablo"; //Valor no vacío
 var_dump(empty($var3));
+echo "<BR>";
 
-$var4 = false;
+$var4 = false; //Valor vacío
 var_dump(empty($var4));
+echo "<BR>";
 
-$var5 = 0;
+$var5 = 0; //Valor vacío
 var_dump(empty($var5));
+echo "<BR>";
 
-$var6 = array();
+$var6 = array(); //Valor no vacio 
 var_dump(empty($var6));
+echo "<BR>";
 
 ?>

0bc91e2ee2f89aa9f22bb437f010f98a06fcae7c
PabloMateosP <pmatpal0105@g.educaand.es>
2023-10-17 18:02:22 +0200

    Subida método delete

--- Tema03/Proyectos/01/02/libs/crud_funciones.php
+++ Tema03/Proyectos/01/02/libs/crud_funciones.php
@@ -14,8 +14,24 @@
 
     // Indicamos que el primer argumento es un array 
     function delete ($tabla = [], $id){
-        // buscar elemento 
-        
+        // Creamos un elemento nulo para posteriormente mostrarlo en mensaje
+        $libroEliminado = null;
+
+        // Comenzamos la búsqueda del libro con el id introducido 
+        foreach ($tabla as $indice => $libro) {
+            if ($libro['id'] == $id) {
+                $libroEliminado = $tabla[$indice];
+                // Gracias al método unset borramos el libro con el id buscado. 
+                unset($libros[$indice]);
+                break;
+            }
+        }
+
+        if ($libroEliminado) {
+            return 'El libro con ID ' . $id . ' ("' . $libroEliminado['titulo'] . '") ha sido eliminado. La tabla ha sido actualizada.';
+        } else {
+            return 'No se encontró ningún libro con el ID ' . $id . '. La tabla no ha sido modificada.';
+        }
     }
 
 ?>

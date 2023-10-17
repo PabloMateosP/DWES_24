979610fdba4df00ce881c2214d2270367e647642
unknown <pablomp1301@gmail.com>
2023-10-17 10:27:09 +0200

    Subida proyecto

---
+++ Tema03/Proyectos/01/02/libs/crud_funciones.php
@@ -0,0 +1,21 @@
+<?php 
+
+    /*
+
+    función: delete()
+    descripcion: elimina un elemento de la tabla
+    parámetros:
+        - tabla
+        - id del elemento que deseo eliminar
+    salida: 
+        - tabla actualizada
+        
+    */
+
+    // Indicamos que el primer argumento es un array 
+    function delete ($tabla = [], $id){
+        // buscar elemento 
+        
+    }
+
+?>

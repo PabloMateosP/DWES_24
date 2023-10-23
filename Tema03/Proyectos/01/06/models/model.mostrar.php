c1d10d9ec25ca7422a8d7cc79a3e1aa68a60328f
unknown <pablomp1301@gmail.com>
2023-10-23 14:58:31 +0200

    PabloMp || Subida tarea 3.11 y anteriores completadas

--- Tema03/Proyectos/01/06/models/model.mostrar.php
+++ Tema03/Proyectos/01/06/models/model.mostrar.php
@@ -6,7 +6,7 @@ $id = $_GET['id'];
 
 $indice_mostrar = buscar_en_tabla($libros, 'id', $id);
 
-if ($indice_mostrar != null ) {
+if ($indice_mostrar !== false ) {
 
     $libro = $libros[$indice_mostrar];
 

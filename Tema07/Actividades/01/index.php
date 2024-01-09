534a58af81272a268a2c43eef55a769def2ab66a
unknown <pablomp1301@gmail.com>
2024-01-09 10:36:24 +0100

    PabloMp - Subida Ejemplos Clase - 09/01/23

---
+++ Tema07/Actividades/01/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 7.1</title>
+</head>
+<body>
+    <h1>Número visitas: <?=$numero_visit ?></h1>
+    <ul>
+        <li>
+            <a href="home.php">Home</a>
+        </li>
+        <li>
+            <a href="about.php">About Me</a>
+        </li>
+        <li>
+            <a href="servicios.php">Servicios</a>
+        </li>
+        <li>
+            <a href="eventos.php">Eventos</a>
+        </li>
+        <li>
+            <a href="close.php">Cerrar</a>
+        </li>
+    </ul>
+
+    <h3>Detalles de la página</h3>
+    <ul>
+        <li>Página Home</li>
+        <li>Sid: <?= ?></li>
+    </ul>
+
+</body>
+</html>

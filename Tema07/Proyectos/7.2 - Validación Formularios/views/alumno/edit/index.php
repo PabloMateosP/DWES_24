ba3d50c82babc6f77a77b1c59982cc5e06c72b76
unknown <pablomp1301@gmail.com>
2024-01-15 14:52:30 +0100

    PabloMp - Subida archivos mostrar errores - 15/01/24

--- Tema07/Proyectos/7.2 - Validación Formularios/views/alumno/edit/index.php
+++ Tema07/Proyectos/7.2 - Validación Formularios/views/alumno/edit/index.php
@@ -4,13 +4,15 @@
 <head>
     <!-- head -->
     <?php require_once("template/partials/head.php") ?>
-    <title><?= $this->title ?></title>
+    <title>
+        <?= $this->title ?>
+    </title>
 </head>
 
 <body>
     <!-- Menú Principal -->
-	<?php require_once("template/partials/menu.php") ?>
-	<br><br><br>
+    <?php require_once("template/partials/menu.php") ?>
+    <br><br><br>
 
     <!-- Capa principal -->
     <div class="container">
@@ -24,35 +26,65 @@
 
             <!-- id oculto -->
             <input type="number" class="form-control" name="id" value="<?= $this->alumno->id ?>" hidden>
-           
+
             <!-- Nombre -->
             <div class="mb-3">
                 <label for="nombre" class="form-label">Nombre</label>
+                <!-- Mostrar posible error -->
                 <input type="text" class="form-control" name="nombre" value="<?= $this->alumno->nombre ?>">
+                <?php if (isset($this->errores['nombre'])): ?>
+                    <span class="form-text text-danger" role="alert">
+                        <?= $this->errores['nombre'] ?>
+                    </span>
+                <?php endif; ?>
             </div>
 
             <!-- Apellidos -->
             <div class="mb-3">
                 <label for="apellidos" class="form-label">Apellidos</label>
                 <input type="text" class="form-control" name="apellidos" value="<?= $this->alumno->apellidos ?>">
+                <!-- Mostrar posible error -->
+                <?php if (isset($this->errores['apellidos'])): ?>
+                    <span class="form-text text-danger" role="alert">
+                        <?= $this->errores['apellidos'] ?>
+                    </span>
+                <?php endif; ?>
             </div>
 
             <!-- Fecha Nacimiento -->
             <div class="mb-3">
                 <label for="fechaNac" class="form-label">Fecha Nacimiento</label>
                 <input type="date" class="form-control" name="fechaNac" value="<?= $this->alumno->fechaNac ?>">
+                <!-- Mostrar posible error -->
+                <?php if (isset($this->errores['fechaNac'])): ?>
+                    <span class="form-text text-danger" role="alert">
+                        <?= $this->errores['fechaNac'] ?>
+                    </span>
+                <?php endif; ?>
             </div>
 
             <!-- Dni -->
             <div class="mb-3">
                 <label for="dni" class="form-label">Dni</label>
                 <input type="text" class="form-control" name="dni" value="<?= $this->alumno->dni ?>">
+                <!-- Mostrar posible error -->
+                <?php if (isset($this->errores['dni'])): ?>
+                    <span class="form-text text-danger" role="alert">
+                        <?= $this->errores['dni'] ?>
+                    </span>
+                <?php endif; ?>
             </div>
 
             <!-- Email -->
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" name="email" value="<?= $this->alumno->email ?>">
+                <!-- Mostrar posible error -->
+                <?php if (isset($this->errores['email'])): ?>
+                    <span class="form-text text-danger" role="alert">
+                        <?= $this->errores['email'] ?>
+                    </span>
+                <?php endif; ?>
             </div>
 
             <!-- Telefono -->
@@ -60,11 +92,16 @@
                 <label for="telefono" class="form-label">Teléfono</label>
                 <input type="tel" class="form-control" name="telefono" value="<?= $this->alumno->telefono ?>">
             </div>
-            
+
             <!-- Población -->
             <div class="mb-3">
                 <label for="poblacion" class="form-label">Población</label>
                 <input type="text" class="form-control" name="poblacion" value="<?= $this->alumno->poblacion ?>">
+                <?php if (isset($this->errores['poblacion'])): ?>
+                    <span class="form-text text-danger" role="alert">
+                        <?= $this->errores['poblacion'] ?>
+                    </span>
+                <?php endif; ?>
             </div>
 
             <!-- Curso Select -->
@@ -72,9 +109,7 @@
                 <label for="id_curso" class="form-label">Curso</label>
                 <select class="form-select" aria-label="Default select example" name="id_curso">
                     <?php foreach ($this->cursos as $data): ?>
-                        <option value="<?= $data->id ?>" 
-                        <?= ($this->alumno->id_curso == $data->id)? 'selected':  null ?>
-                        >
+                        <option value="<?= $data->id ?>" <?= ($this->alumno->id_curso == $data->id) ? 'selected' : null ?>>
                             <?= $data->curso ?>
                         </option>
                     <?php endforeach; ?>
@@ -99,7 +134,7 @@
 
     <!-- javascript bootstrap 532 -->
     <?php include 'template/partials/javascript.php' ?>
-    
+
 </body>
 
 </html>

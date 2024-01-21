a13be7ac6d44746c60bb9e9a0f9395aa1aa85081
PabloMateosP <pmatpal0105@g.educaand.es>
2024-01-21 18:04:27 +0100

    PabloMP - Cambio localización documentos y Terminamos 7.3 y 7.1 - 21/01/24

---
+++ Tema07/Proyectos/7.1-Validacion_Gesbank/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+# Ruta absoluta
+define('URL', 'http://localhost/DWES/Tema07/Proyectos/7.1-Validacion_Gesbank/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>

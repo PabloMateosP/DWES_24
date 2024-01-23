37fb238f7d1fe5c445ebb7ca39191ec274204e81
unknown <pablomp1301@gmail.com>
2024-01-23 09:04:10 +0100

    PabloMP - Subida Actividad 7.4 y comienzo 7.5 - 23/01/24

---
+++ Tema07/Actividades/7.4-Gestion_Perfiles/config/config.php
@@ -0,0 +1,13 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/DWES/Tema07/Actividades/7.4-Gestion_Perfiles/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'fp');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
---
+++ Tema07/Actividades/7.5-Menu_Usuario/config/config.php
@@ -0,0 +1,13 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/DWES/Tema07/Actividades/7.5-Menu_Usuario/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'fp');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');

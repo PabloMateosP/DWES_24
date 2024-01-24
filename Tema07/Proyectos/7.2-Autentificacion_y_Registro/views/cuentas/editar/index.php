a73fce5fb37cfb9da50e4fc80592ee3c648f73ec
unknown <pablomp1301@gmail.com>
2024-01-24 14:49:09 +0100

    PabloMP - Subida Tarea 7.2 Casi Finalizada - 24/01/24

--- Tema07/Proyectos/7.2-Autentificacion_y_Registro/views/cuentas/editar/index.php
+++ Tema07/Proyectos/7.2-Autentificacion_y_Registro/views/cuentas/editar/index.php
@@ -9,7 +9,7 @@
 
 <body>
     <!-- menu principal fijo superior -->
-    <?php require_once "template/partials/menu.php"; ?>
+    <?php require_once "template/partials/menuAut.php"; ?>
     <!-- capa principal -->
     <div class="container">
         <!-- cabecera o título -->

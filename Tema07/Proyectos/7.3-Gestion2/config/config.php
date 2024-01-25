ace08ae4f05506ba6cd8b7c18f7077b53c5222af
PabloMateosP <pmatpal0105@g.educaand.es>
2024-01-25 18:29:28 +0100

    PabloMp - Subida Tarea 7.3 Gestión (Finalizada) - 25/01/24

---
+++ Tema07/Proyectos/7.3-Gestion2/config/config.php
@@ -0,0 +1,15 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/DWES/Tema07/Proyectos/7.3-Gestion2/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
---
+++ Tema07/Proyectos/7.3-Gestion2/config/privilegios.php
@@ -0,0 +1,26 @@
+<?php
+    /**
+     * # Perfiles
+     * 1 - Administrador
+     * 2 - Editor
+     * 3 - Registrado
+     * 
+     */
+
+    // Permisos clientes
+    $GLOBALS['clientes']['main'] = [1,2,3];
+    $GLOBALS['clientes']['new'] = [1,2];
+    $GLOBALS['clientes']['edit'] = [1,2];
+    $GLOBALS['clientes']['delete'] = [1];
+    $GLOBALS['clientes']['show'] = [1,2,3];
+    $GLOBALS['clientes']['filter'] = [1,2,3];
+    $GLOBALS['clientes']['order'] = [1,2,3];
+
+    // Permisos cuentas
+    $GLOBALS['cuentas']['main'] = [1,2,3];
+    $GLOBALS['cuentas']['new'] = [1,2];
+    $GLOBALS['cuentas']['edit'] = [1,2];
+    $GLOBALS['cuentas']['delete'] = [1];
+    $GLOBALS['cuentas']['show'] = [1,2,3];
+    $GLOBALS['cuentas']['filter'] = [1,2,3];
+    $GLOBALS['cuentas']['order'] = [1,2,3];

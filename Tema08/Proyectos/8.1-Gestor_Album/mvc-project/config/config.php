f1558c78d9e56615e00e70b8cd8fe3f5ad44a9af
unknown <pablomp1301@gmail.com>
2024-02-05 14:54:42 +0100

    PabloMP - Subida Tarea 8.1 (Iniciado) - 05/02/24

---
+++ Tema08/Proyectos/8.1-Gestor_Album/mvc-project/config/config.php
@@ -0,0 +1,13 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/DWES/Tema08/Proyectos/8.1-Gestor_Album/mvc-project/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'album');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');

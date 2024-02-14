d8f986b3bf56a3f0e409df93b1e9cefb40a47224
unknown <pablomp1301@gmail.com>
2024-02-14 14:51:05 +0100

    PabloMP - Subida Ejemplos Tema 10 (Falta Ver Otros Correctos) - 14/02/24

--- Tema10/Ejemplos/04/index.php
+++ Tema10/Ejemplos/04/index.php
@@ -1,10 +1,7 @@
 <?php
-/**
- * Ejemplo 04
- * Envio de correo con PHPMailer (OutLook)
- */
 
-// Cargamos la clase PHPMailer, para procesar email con PHPMailer
+// Procesar email con PHPMailer
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -12,59 +9,65 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-// Objeto clase PHPMailer
-$email = new PHPMailer(true);
+// Creo objeto clase PHPMailer
+$mail = new PHPMailer(true);
 
-// En caso de error se lanza Exception
+// En caso de error lanza Exception
 try {
 
-    // Configurar juego de caracteres
-    $email->CharSet = "UTF-8";
-    $email->Encoding = "quoted-printable";
+    // Configuración juego caracteres
+    $mail->CharSet = "UTF-8";
+    $mail->Encoding = "quoted-printable";
 
-    // STMP Outlook
-    $email->isSMTP();
-    $email->Host = "smtp.gmail.com";
-    $email->Port = "587";
+    // Credenciales SMPT gmail
+    $mail->Username = "partypat1301@gmail.com";
+    $mail->Password = "***************"; // Contraseña de aplicaciones gmail
 
-    //TLS
-    $email->SMTPSecure = 'tls';
-    $email->SMTPAuth = true;
-    $email->Username = "partypat1301@gmail.com";
-    $email->Password = "**************";
+    // Configuración SMPT gmail
+    $mail->SMTPDebug = 2;                                       //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = 'smtp.gmail.com';                       //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication                             //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // tls Enable implicit TLS encryption
+    $mail->Port = 25; // Suele ser el puerto o el 25 o el 587 aunque unas veces funciona uno otras el otro
 
-    // Creación del mensaje
-    $email->setFrom("partypat1301@gmail.com", '*********');
-    $email->addAddress("partypat1301@gmail.com", 'Annita Maxwinn');
-    $email->Subject = "PHPMailer";
-    $email->Body = "
-    <!DOCTYPE html>
-    <html lang=\"es\">
-        <head>
-            <meta charset=\"UTF-8\">
-            <title>¡Hola, mundo!</title>
-        </head>
-        <body>
-            <h1>¡Hola, mundo!</h1>
-            <p>¿Sabías que los programadores tienen una frase especial para cada día? Hoy, nuestra frase es:</p>
-            <blockquote><em>\"Los programadores no aman la limpieza, pero odian la suciedad.\"</em></blockquote>
-            <p>¡Esperamos que hayas tenido un buen día y estés listo para un nuevo día lleno de código y risas!</p>
-            <p>Saludos desde el equipo de desarrollo.</p>
-        </body>
-    </html>";
-    $email->AltBody = "Este es un mensaje HTML. Si ves este mensaje, significa que tu cliente de correo no soporta HTML.";
+    //Cabecera del email
+    $destinatario = 'partypat1301@gmail.com';
+    $remitente = 'partypat1301@gmail.com';
+    $asunto = "Email con PHPMailer";
+    $mensaje = "
+    <h1>Lorem ipsum dolor sit amet</h1>
+    <b>Cádiz</b> consectetur adipiscing elit. 
+    Curabitur pellentesque metus sit amet tellus placerat, quis interdum nibh fermentum. 
+    Aenean porttitor arcu sit amet orci placerat aliquet. Sed in ex sed tellus volutpat 
+    finibus ac sed nibh. Mauris vitae magna nunc. Vivamus ornare massa imperdiet purus 
+    rhoncus, quis ultrices odio faucibus. Duis efficitur id urna nec semper. 
+    Vivamus rutrum ac felis id egestas. Duis consequat, eros vel luctus eleifend, 
+    ligula nibh hendrerit nibh, eget ultrices sapien eros ut ipsum. 
+    Donec vel sollicitudin elit, ut elementum diam.
+    ";
 
-    // Comprobación de errores
-    if (!$email->send()) {
-        echo "Error PHPMailer";
-        echo "<pre>";
-        print_r($email);
-        exit();
-    } else {
-        echo "success";
-        exit();
-    }
+    $mail->setFrom($remitente, 'Paco');
+    $mail->addAddress($destinatario, 'Pepe');
+    $mail->addReplyTo($remitente, 'Paco Fiestas');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
-} catch (Exception $th) {
-    echo 'Error: PHPMailer ha sufrido un error critico';
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $asunto;
+    $mail->Body = $mensaje;
+
+    // Enviamos el mensaje
+    $mail->send();
+
+    echo 'Message has been sent';
+
+} catch (Exception $e) {
+
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }

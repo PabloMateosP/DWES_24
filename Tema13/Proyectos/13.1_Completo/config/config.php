a96ba2a80929055ac5682613d0e138a0c4f9bd37
PabloMateosP <pmatpal0105@g.educaand.es>
2024-03-04 20:41:50 +0100

    PabloMP - Subida inicio 13.1 - 04/03/24

---
+++ Tema13/Proyectos/13.1_Completo/config/config.php
@@ -0,0 +1,12 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/DWES/Tema13/Proyectos/13.1_Completo/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');

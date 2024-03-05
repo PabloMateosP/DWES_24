5d27df3551fe481806f62aa6a673987edf53e4e7
unknown <pablomp1301@gmail.com>
2024-03-05 10:34:47 +0100

    PabloMP - Subida Tema 12 avances - 05/03/24

--- Tema12/Proyectos/09/laravel-09/database/seeders/StudentSeeder.php
+++ Tema12/Proyectos/09/laravel-09/database/seeders/StudentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StudentSeeder extends Seeder
 {
@@ -13,5 +14,17 @@ class StudentSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('students')->insert([
+            [
+                'name' => 'Pablo',
+                'last_name' => 'Mateos Palas',
+                'birth_date' => '1885/05/01',
+                'phone' =>  '639874563',
+                'email' => 'pablo@gmail.com',
+                'dni' =>  '12345678A',
+                'city' =>  'Madrid',
+                'course_id' => 1
+            ]
+        ]);
     }
 }

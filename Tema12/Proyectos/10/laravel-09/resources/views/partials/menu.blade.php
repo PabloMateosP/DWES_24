f6f4b638e40625c1fb729c9aa8fd35bbfd055167
unknown <pablomp1301@gmail.com>
2024-03-11 14:58:27 +0100

    PabloMP - Subida Tarea Clase - 11/03/24

--- Tema12/Proyectos/10/laravel-09/resources/views/partials/menu.blade.php
+++ Tema12/Proyectos/10/laravel-09/resources/views/partials/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="#">Laravel GesAlumnos 1.0</a>
+      <a class="navbar-brand" href="{{ route('alumnos.index')}}">Laravel GesAlumnos 1.0</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
